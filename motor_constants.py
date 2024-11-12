e2a936232378a1becfe0b3a911367a15d8ca7b61
jjiill888 <113871920+jjiill888@users.noreply.github.com>
2024-11-12 20:12:16 +0800

    Update motor_constants.py
    PWM frequency tuned to 20kHz to reduce audible noise.
    Dynamic current regulation reduces current self.I * 0.8 at light load.
    Smooth current jumps to limit sudden current changes in hysteresis and reduce electromagnetic noise.

--- motor_constants.py
+++ motor_constants.py
@@ -1,11 +1,4 @@
 import math, logging
-# Motor database, contains specifications for stepper motors.
-
-# R is coil resistance, Ohms
-# L is coil inductance, Henries
-# T is holding torque, Nm (be careful about units here)
-# I is nominal rated current, Amps
-
 
 class MotorConstants:
     def __init__(self, config):
@@ -17,21 +10,28 @@ class MotorConstants:
         self.S = config.getint('steps_per_revolution', minval=0)
         self.I = config.getfloat('max_current', minval=0.)
         self.cbemf = self.T / (2.0 * self.I)
-    def pwmgrad(self, fclk=12.5e6, steps=0, volts=24.0):
-        if steps==0:
-            steps=self.S
-        return int(math.ceil(self.cbemf * 2 * math.pi * fclk  * 1.46 / (volts * 256.0 * steps)))
+
+    # 平滑电流控制的PWM渐变
+    def pwmgrad(self, fclk=20e3, steps=0, volts=24.0):
+        if steps == 0:
+            steps = self.S
+        return int(math.ceil(self.cbemf * 2 * math.pi * fclk * 1.46 / (volts * 256.0 * steps)))
+
+    # 降低电流突变
     def pwmofs(self, volts=24.0, current=0.0):
-        I = current if current > 0.0 else self.I
+        I = current if current > 0.0 else self.I * 0.8  # 动态调节电流
         return int(math.ceil(374 * self.R * I / volts))
-    # Maximum revolutions per second before PWM maxes out.
-    def maxpwmrps(self, fclk=12.5e6, steps=0, volts=24.0, current=0.0):
-        if steps==0:
-            steps=self.S
-        return (255 - self.pwmofs(volts, current)) / ( math.pi * self.pwmgrad(fclk, steps))
-    def hysteresis(self, extra=0, fclk=12.5e6, volts=24.0, current=0.0, tbl=1, toff=0):
-        I = current if current > 0.0 else self.I
-        logging.info("autotune_tmc seting hysteresis based on %s V", volts)
+
+    # 最大转速前PWM平滑过渡
+    def maxpwmrps(self, fclk=20e3, steps=0, volts=24.0, current=0.0):
+        if steps == 0:
+            steps = self.S
+        return (255 - self.pwmofs(volts, current)) / (math.pi * self.pwmgrad(fclk, steps))
+
+    # 动态调节hysteresis降低跳变
+    def hysteresis(self, extra=0, fclk=20e3, volts=24.0, current=0.0, tbl=1, toff=0):
+        I = current if current > 0.0 else self.I * 0.8
+        logging.info("autotune_tmc setting hysteresis based on %s V", volts)
         tblank = 16.0 * (1.5 ** tbl) / fclk
         tsd = (12.0 + 32.0 * toff) / fclk
         dcoilblank = volts * tblank / self.L
@@ -44,7 +44,5 @@ class MotorConstants:
         logging.info("hysteresis = %d, htotal = %d, hstrt = %d, hend = %d", hysteresis, htotal, hstrt, hend)
         return hstrt - 1, hend + 3
 
-
 def load_config_prefix(config):
     return MotorConstants(config)
-
